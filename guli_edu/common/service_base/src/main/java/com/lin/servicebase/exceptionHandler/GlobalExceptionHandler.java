7efd646b7d6116b8297741e7902cced41ff5aaac
lingongxin <1277572102@qq.com>
2020-12-24 10:31:24 +0800

    统一日志的配置

--- guli_edu/common/service_base/src/main/java/com/lin/servicebase/exceptionHandler/GlobalExceptionHandler.java
+++ guli_edu/common/service_base/src/main/java/com/lin/servicebase/exceptionHandler/GlobalExceptionHandler.java
@@ -7,6 +7,8 @@ package com.lin.servicebase.exceptionHandler;
  */
 
 import com.lin.commonutils.Result;
+import com.lin.servicebase.utils.ExceptionUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,11 +19,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
  3
  */
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     @ResponseBody
     public Result error(Exception e){
+
+        log.error(ExceptionUtil.getMessage(e));
+        log.error(e.getMessage());
         e.printStackTrace();
         return Result.error();
     }
